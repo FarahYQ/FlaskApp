b625b08f99fda26a0ee2bc4e62e66f1f0fb40cf2
Farah Quader <fyquader@gmail.com>
2019-03-10 14:52:13 -0700

    Connect to MySQL database

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, request
-# from flask_mysql import MySQL
+from flask_mysql import MySQL
+import yaml
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+# Configure MySQL database
+db = yaml.load(open('dbconfig.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_details = request.form
+        name = user_details['name']
+        age = user_details['age']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (name, age))
+        mysql.connection.commit()
+        cur.close()
     return render_template('index.html')
 
 if __name__ == "__main__":
