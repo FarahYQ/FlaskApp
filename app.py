7fefa6acfdb3b50745f12b9495553be695a707af
Farah Quader <fyquader@gmail.com>
2019-03-12 21:55:51 -0700

    Switch to flask_sqlalchemy for compatibility

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
-from flask import Flask, render_template, request
-from flask_mysql import MySQL
-import yaml
-
+from flask import Flask, render_template, request, jsonify
+import dbconfig
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Configure MySQL database
-db = yaml.load(open('dbconfig.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-mysql = MySQL(app)
+db_info = dbconfig.db_info
+user = db_info['mysql_user']
+pw = db_info['mysql_password']
+host = db_info['mysql_host']
+db_name = db_info['mysql_db']
+db_uri = f'mysql://{user}:{pw}@{host}/{db_name}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+db = SQLAlchemy(app)
 
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user_details = request.form
-        name = user_details['name']
-        age = user_details['age']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (name, age))
-        mysql.connection.commit()
-        cur.close()
+        # Fetch the form data
+        print(request.form)
+        return jsonify({'name': request.form['name'], 'age': int(request.form['age'])})
     return render_template('index.html')
+
+@app.route("/users/<int:num>")
+def showUser(num):
+    return jsonify({'userId': num})
 
 if __name__ == "__main__":
     app.run(debug = True)
